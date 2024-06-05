cfb78034cd6088e2a9d1a8561ed860a00dde177a
cpaul <colinpaul@gmail.com>
2024-06-05 15:31:39 +0100

    adding dash and layout customisation

--- golf_app.py
+++ golf_app.py
@@ -10,3 +10,44 @@ data = (
     .assign(date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
     .sort_values(by="Date")
 )
+
+app = Dash(__name__)
+
+# app.layout = html.Div(
+#     children=[
+#         html.H1(children="Avocado Analytics"),
+#         html.P(
+#             children=(
+#                 "Analyze the behavior of avocado prices and the number"
+#                 " of avocados sold in the US between 2015 and 2018"
+#             ),
+#         ),
+#         dcc.Graph(
+#             figure={
+#                 "data": [
+#                     {
+#                         "x": data["Date"],
+#                         "y": data["AveragePrice"],
+#                         "type": "lines",
+#                     },
+#                 ],
+#                 "layout": {"title": "Average Price of Avocados"},
+#             },
+#         ),
+#         dcc.Graph(
+#             figure={
+#                 "data": [
+#                     {
+#                         "x": data["Date"],
+#                         "y": data["Total Volume"],
+#                         "type": "lines",
+#                     },
+#                 ],
+#                 "layout": {"title": "Avocados Sold"},
+#             },
+#         ),
+#     ]
+# )
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
