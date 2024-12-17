2faaae0d9fa95b7dea3b2d7d22db046884aec671
cpaul <colinpaul@gmail.com>
2024-12-17 15:09:46 +0000

    initial_edits

--- golf_app.py
+++ golf_app.py
@@ -1,6 +1,8 @@
 # golf_app.py
-# application to monitor impact of fitness levels on golf game and general health metrics
-# test from home 4/9/24
+# application to monitor golf statistics from Garmin and Arccos
+# provide correlation with health statistics such as RHR + Avg Ball Speed
+# show that has you get fitter you hit the ball further
+# update from work 17/12/24
 import pandas as pd
 from dash import Dash, dcc, html
 
@@ -13,41 +15,41 @@ data = (
 
 app = Dash(__name__)
 
-# app.layout = html.Div(
-#     children=[
-#         html.H1(children="Avocado Analytics"),
-#         html.P(
-#             children=(
-#                 "Analyze the behavior of avocado prices and the number"
-#                 " of avocados sold in the US between 2015 and 2018"
-#             ),
-#         ),
-#         dcc.Graph(
-#             figure={
-#                 "data": [
-#                     {
-#                         "x": data["Date"],
-#                         "y": data["AveragePrice"],
-#                         "type": "lines",
-#                     },
-#                 ],
-#                 "layout": {"title": "Average Price of Avocados"},
-#             },
-#         ),
-#         dcc.Graph(
-#             figure={
-#                 "data": [
-#                     {
-#                         "x": data["Date"],
-#                         "y": data["Total Volume"],
-#                         "type": "lines",
-#                     },
-#                 ],
-#                 "layout": {"title": "Avocados Sold"},
-#             },
-#         ),
-#     ]
-# )
+app.layout = html.Div(
+#      children=[
+#          html.H1(children="Golf Stats Analytics"),
+# #         html.P(
+# #             children=(
+# #                 "Analyze the behavior of avocado prices and the number"
+# #                 " of avocados sold in the US between 2015 and 2018"
+# #             ),
+# #         ),
+# #         dcc.Graph(
+# #             figure={
+# #                 "data": [
+# #                     {
+# #                         "x": data["Date"],
+# #                         "y": data["AveragePrice"],
+# #                         "type": "lines",
+# #                     },
+# #                 ],
+# #                 "layout": {"title": "Average Price of Avocados"},
+# #             },
+# #         ),
+# #         dcc.Graph(
+# #             figure={
+# #                 "data": [
+# #                     {
+# #                         "x": data["Date"],
+# #                         "y": data["Total Volume"],
+# #                         "type": "lines",
+# #                     },
+# #                 ],
+# #                 "layout": {"title": "Avocados Sold"},
+# #             },
+# #         ),
+# #     ]
+)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
