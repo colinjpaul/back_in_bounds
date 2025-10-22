f4debc4e19f65761a47e02f773be692c34fb6b30
cpaul <colinpaul@gmail.com>
2025-10-22 17:09:44 +0100

    renaming and reformat data

--- golf_app.py
+++ golf_app.py
@@ -2,53 +2,12 @@
 # application to monitor golf statistics from Garmin and Arccos
 # provide correlation with health statistics such as RHR + Avg Ball Speed
 # show that as you get fitter you hit the ball further
-# update from work 17/12/24
+# update 22-10-25
 import pandas as pd
 from dash import Dash, dcc, html
 
 data = (
     pd.read_csv("garmin_data.csv")
-    .query("Hole == '1' and Score == '5'")
-    .assign(date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
-    .sort_values(by="Hole")
+    .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
+    .sort_values(by="Date")
 )
-
-app = Dash(__name__)
-
-app.layout = html.Div(
-    children=[
-        html.H1(children="Golf Stats Analytics"),
-        html.P(
-            children=(
-                "application to monitor golf statistics from Garmin and Arccos"
-            ),
-        ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Date"],
-                        "y": data["AveragePrice"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Average Price of Golf Balls"},
-            },
-        ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Date"],
-                        "y": data["Total Volume"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Golf Balls Sold"},
-            },
-        ),
-    ]
-)
-
-if __name__ == "__main__":
-    app.run_server(debug=True)
